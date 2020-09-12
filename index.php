b0cccab3979c08b3dac4e064b1ceef917a9bff4f
magvisjerez <magvis.jerez@yahoo.com>
2020-09-11 20:44:31 -0500

    Create index.php
    se crea ejemplo de clase

---
+++ index.php
@@ -0,0 +1,53 @@
+<?php
+
+$producto1='arroz';
+$producto2='huevos';
+$producto3='frijoles';
+$producto4='panela';
+$producto5='cafe';
+$producto6='pastas';
+$producto7='media guaro';
+$producto8='papas';
+$producto9='carne';
+$producto10='sal';
+
+$precio1=6000;
+$precio2=10000;
+$precio3=8000;
+$precio4=4000;
+$precio5=12500;
+$precio6=6000;
+$precio7=15000;
+$precio8=6000;
+$precio9=25000;
+$precio10=800;
+
+$total=$precio1+$precio2+$precio3+$precio4+$precio5+$precio6+$precio7+$precio8+$precio9+$precio10;
+
+//echo('el total de el mercado es:' .$total);
+
+$total=50000;
+
+if($total<=80000)
+{
+    echo('gracias por su compra');
+}else
+{
+    echo('pida el fiado');
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
